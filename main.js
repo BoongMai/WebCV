41c2a2bd8b62f8222b5263184615189afc9e8567
BoongMai <thanhduypham9@gmail.com>
2022-10-31 23:33:06 +0700

    days2

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const mainPage = document.getElementById('main')
 
-console.log(mainPage);
-
 const chk = document.getElementById('chk');
 const labelDark = document.querySelector('.label')
 
+const myHeader = document.getElementById('myHeader')
+const sticky = myHeader.offsetTop
 
+console.log("sticky: " ,sticky);
+console.log("window.pageYOffset: " ,window.pageYOffset);
+
+
+console.log('myHeader: ', myHeader);
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
-	console.log(labelDark);
 	labelDark.classList.toggle('lable__dark-mode')
 });
+
+// Header
+window.addEventListener('scroll', () => {
+	if (window.pageYOffset > sticky) {
+		myHeader.classList.add("sticky");
+	  } else {
+		myHeader.classList.remove("sticky");
+	  }
+})
+
+
+// Canvas 
